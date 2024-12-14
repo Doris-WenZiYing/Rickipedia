272b8dc37378ba8fcb6038be7dc216694730a364
Doris <doris070714@gmail.com>
2024-12-14 09:25:46 +0800

    Initial Commit

---
+++ Rickipedia/RickipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickipediaApp.swift
+//  Rickipedia
+//
+//  Created by App_team on 2024/12/14.
+//
+
+import SwiftUI
+
+@main
+struct RickipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
